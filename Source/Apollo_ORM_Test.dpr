4b28f1f7bf6ffcb14e3b2478415aaa2c21d1bccb
apollo.delphi.lib@gmail.com <apollo.delphi.lib@gmail.com>
2021-02-21 21:57:39 +0300

    dev

--- Source/Apollo_ORM_Test.dpr
+++ Source/Apollo_ORM_Test.dpr
@@ -8,7 +8,10 @@ uses
   DUnitX.Loggers.Xml.NUnit,
   DUnitX.TestFramework,
   Apollo_ORM in 'Apollo_ORM.pas',
-  tstYourUnitName in 'tstYourUnitName.pas';
+  tstYourUnitName in 'tstYourUnitName.pas',
+  Apollo_DB_Core in '..\Vendors\Apollo_DB_Core\Source\Apollo_DB_Core.pas',
+  Apollo_Helpers in '..\Vendors\Apollo_Helpers\Source\Apollo_Helpers.pas',
+  Apollo_Types in '..\Vendors\Apollo_Types\Apollo_Types.pas';
 
 begin
   Application.Initialize;
