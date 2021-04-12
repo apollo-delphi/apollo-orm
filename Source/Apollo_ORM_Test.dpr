d8fdccc3943e02de24180c855b3566eb3528ea97
alezzle <niekras@tut.by>
2021-04-12 14:02:42 +0300

    dev

--- Source/Apollo_ORM_Test.dpr
+++ Source/Apollo_ORM_Test.dpr
@@ -9,10 +9,10 @@ uses
   DUnitX.TestFramework,
   Apollo_ORM in 'Apollo_ORM.pas',
   tstYourUnitName in 'tstYourUnitName.pas',
-  Apollo_Binding_Core in '..\Vendors\Apollo_Binding_Core\Apollo_Binding_Core.pas',
   Apollo_DB_Core in '..\Vendors\Apollo_DB_Core\Apollo_DB_Core.pas',
   Apollo_Helpers in '..\Vendors\Apollo_Helpers\Apollo_Helpers.pas',
-  Apollo_Types in '..\Vendors\Apollo_Types\Apollo_Types.pas';
+  Apollo_Types in '..\Vendors\Apollo_Types\Apollo_Types.pas',
+  Apollo_Binding_Core in '..\Vendors\Apollo_Binding_Core\Apollo_Binding_Core.pas';
 
 begin
   Application.Initialize;
